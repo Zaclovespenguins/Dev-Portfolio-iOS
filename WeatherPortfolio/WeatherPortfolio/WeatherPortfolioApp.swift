29ed5b157db2a9e8739062d987bd2943c668d81b
Zaclovespenguins <46504778+Zaclovespenguins@users.noreply.github.com>
2024-12-08 10:20:26 -0700

    Weather App project creation

---
+++ WeatherPortfolio/WeatherPortfolio/WeatherPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherPortfolioApp.swift
+//  WeatherPortfolio
+//
+//  Created by Zachary Reyes on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
